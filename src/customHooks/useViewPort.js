28073af1454e2ab089708b3622363ef63083002f
Michael Daigler <mdaigler2601@gmail.com>
2021-03-07 09:01:24 -0500

    Updates

--- src/customHooks/useViewPort.js
+++
@@ -1,3 +0,0 @@
-import React, {useState, useEffect, useContext} from "react";
-import viewportContext from "../Context/ViewportProvider";
-import ViewportProvider, {} from "../Context/ViewportProvider";
